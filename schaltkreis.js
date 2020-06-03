dea074ea16eaaeeeb5856b4b96b5bf21c558b07b
mino <konformitaet@gmail.com>
2020-06-03 19:39:00 +0200

    small fixes, hints

--- schaltkreis.js
+++ schaltkreis.js
@@ -1,22 +1,24 @@
 const correctArray = ["wenn", "ich", "nicht", "sehe", "dass", "ich", "blind", "bin", "dann", "bin", "ich", "blind", "wenn", "ich", "aber", "sehe", "dass", "ich", "blind", "bin", "dann", "sehe", "ich"];
 let quoteIndex = 0;
-const correctQuote = "wenn ich nicht sehe, dass ich blind bin, dann bin ich blind; wenn ich aber sehe, dass ich blind bin, dann sehe ich."
+const correctQuote =
+    "wenn ich nicht sehe,\n \t\tdass ich blind bin,\n dann bin ich blind;\n \t\twenn ich aber sehe,\n dass ich blind bin,\n \t\t\tdann sehe ich.";
 const sourceText = "- heinz von förster";
 let lastWord = "";
+let hintTimerRunning = false;
+let hintTime;
+let hintTimeAmount = 4000;
+let firstSolve = false;
 
 function speak(elem) {
-  var msg = new SpeechSynthesisUtterance();
-  var synth = window.speechSynthesis;
-  var voices = synth.getVoices();
-  console.log(voices);
+  let msg = new SpeechSynthesisUtterance();
+  let synth = window.speechSynthesis;
+  let voices = synth.getVoices();
   msg.text = elem.innerText;
   msg.lang = 'de-DE';
   msg.voice = voices[2];
-
   msg.volume = 1; // 0 to 1
-  msg.rate = 0.9; // 0.1 to 10
+  msg.rate = 1.1; // 0.1 to 10
   msg.pitch = 1; //0 to 2
-
 
   msg.start = function(e) {
     elem.classList.add("highlight");
@@ -33,31 +35,69 @@ function speak(elem) {
     quoteIndex++;
     quote.innerText = truncate(correctQuote, quoteIndex);
   } else if(quoteIndex < 23) {
-    quoteIndex = 0;
-    quote.innerText = "";
+    hintTimeAmount = Math.max(hintTimeAmount - 50, 500);
+    console.log(hintTimeAmount);
+    if (correctArray[0] == elem.innerText) {
+      quoteIndex = 1;
+      quote.innerText = truncate(correctQuote, quoteIndex);
+    } else {
+      quoteIndex = 0;
+      quote.innerText = "";
+    }
   }
-
-  if(quoteIndex == 23) {
-    source.innerText = sourceText;
-  }
-
-  // console.log("speaking", msg);
-
+  
+  if(quoteIndex == 1) speechSynthesis.cancel();
   speechSynthesis.speak(msg);
   lastWord = elem.innerText;
+  if(hintTimerRunning) {
+    hintTimerRunning = false;
+    // console.log("clearing hint timer");
+    clearTimeout(hintTime);
+  }
+  
+  hintTime = setTimeout(hint, hintTimeAmount);
+  hintTimerRunning = true;
+  
+  // finished quote
+  if(quoteIndex == 23 && !firstSolve) {
+    source.innerText = sourceText;
+    let quot = new SpeechSynthesisUtterance();
 
+    quot.text = elem.innerText;
+    quot.lang = 'de-DE';
+    quot.voice = voices[2];
+    quot.volume = 1; // 0 to 1
+    quot.rate = 1.1; // 0.1 to 10
+    quot.pitch = 1; //0 to 2
+    quot.text = "! zitiert nach heinz von förster";
+    speechSynthesis.speak(quot);
+    firstSolve = true;
+  }
 
 }
 
+// gives hint for next word after timer ran out
+function hint() {
+  console.log("giving hint");
+  for(let i = 0; i < schaltWord.length; i++) {
+    if(correctArray[quoteIndex] == schaltWord[i].innerText) {
+      schaltWord[i].classList.add("blink_me");
+    }
+  }
+  hintTimerRunning = false;
+}
 
 // event listeners
 const schaltWord = document.getElementsByClassName("schaltWord");
-console.log(schaltWord);
 for(let i = 0; i < schaltWord.length; i++) {
   schaltWord[i].addEventListener("mouseenter", function( event ) {
-    console.log(schaltWord[i].innerText);
+    // console.log("selected", schaltWord[i].innerText);
+    // remove hint
+    for(let j = 0; j < schaltWord.length; j++) {
+      schaltWord[j].classList.remove("blink_me");
+    }
+    // prevent duplicate utterance
     if(lastWord != schaltWord[i].innerText) {
-      console.log("uhmm...", lastWord, schaltWord[i].innerText);
       speak(schaltWord[i]);
     }
   });
@@ -70,4 +110,5 @@ function truncate(str, no_words) {
 //show quote on success
 const quote = document.getElementById("quoteshow");
 const source = document.getElementById("source");
-// quote.innerText = correctQuote;
+
+hint();
